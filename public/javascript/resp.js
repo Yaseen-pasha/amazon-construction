f602ca98cc49e351448755485924957fd397d7b1
Yaseen <yaseenpadha7296@gmail.com>
2022-01-14 19:32:11 +0530

    adding preloader

--- public/javascript/resp.js
+++ public/javascript/resp.js
@@ -8,8 +8,11 @@ burger.addEventListener('click', ()=>{
     navList.classList.toggle('v-class');
     navbar.classList.toggle('h-nav');
 })
-
     var burger = document.getElementById("burger");
     burger.onclick = function(){
     burger.classList.toggle("openburger");
 }
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
