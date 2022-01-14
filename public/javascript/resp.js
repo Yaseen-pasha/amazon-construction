60074aa74d1de757733800e0e1afac931f0a667a
Yaseen <yaseenpadha7296@gmail.com>
2022-01-14 18:14:45 +0530

    make it better

--- public/javascript/resp.js
+++ public/javascript/resp.js
@@ -8,3 +8,8 @@ burger.addEventListener('click', ()=>{
     navList.classList.toggle('v-class');
     navbar.classList.toggle('h-nav');
 })
+
+    var burger = document.getElementById("burger");
+    burger.onclick = function(){
+    burger.classList.toggle("openburger");
+}
